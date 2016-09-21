cacd8d0b0228d9055c5ece3c1c8bd84aa288b402
thegracefulolive <g.c.o.1998@gmail.com>
2016-09-21 12:07:35 -0400

    added new scene w/bombs

---
+++ Assets/scripts/DestryOnClick.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+
+function OnMouseDown () {
+	if( gameObject.transform.position.y > -2 && gameObject.transform.position.y < 3.5)
+	{
+		Destroy(gameObject);
+	}
+}
+
+function Update () {
+
+}
