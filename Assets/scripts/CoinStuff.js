9e0a8eeddd4de50ad64133eb859ab1af6c060962
thegracefulolive <g.c.o.1998@gmail.com>
2016-09-21 11:43:56 -0400

    a lot of new stuff

---
+++ Assets/scripts/CoinStuff.js
@@ -0,0 +1,11 @@
+﻿#pragma strict
+
+public var points : int = 5;
+public var pickedUpBy : String = "Player";
+
+function OnTriggerEnter2D(other : Collider2D) {
+	if( other.CompareTag(pickedUpBy)){
+	Debug.Log("WOW! Worth " + points + "points!");
+	Destroy(gameObject);
+	}
+}
---
+++ Assets/scripts/InstantiateGobj.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+public var gObj: GameObject;
+
+function Start () {
+CreateRandom();//code gets run once
+}
+
+function CreateRandom() { // create a random number bewteen 2 values
+
+	var x: float;
+
+	var y: float;
+
+	x=Random.Range(-7.0, 7.0);
+
+	y= 6;
+
+	Instantiate( gObj, Vector3(x, y, 0), Quaternion.identity); //what object will we clone, where will it start, and do we want it to rotate
+}
